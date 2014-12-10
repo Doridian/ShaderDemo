19af44eaf38061a7cc2a15c6825e457db9bfa840
Mark Dietzer <mriq91@gmail.com>
2014-12-10 23:33:10 +0100

    Order things a bit here

--- src/de/doridian/crtdemo/basic/BasicIO.java
+++ src/de/doridian/crtdemo/basic/BasicIO.java
@@ -2,10 +2,14 @@ package de.doridian.crtdemo.basic;
 
 public interface BasicIO {
     public String readLine();
+
     public void clearLine(int line);
     public void clearScreen();
+
     public int getLines();
     public int getColumns();
+
     public void print(Object obj);
+
     public void setCursor(int column, int line);
 }
