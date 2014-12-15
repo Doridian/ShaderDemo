7f1c13283b665f6223a7d741944b4d3522d68cb5
Mark Dietzer <mriq91@gmail.com>
2014-12-15 20:14:21 +0100

    Okay, do this instead

--- src/de/doridian/crtdemo/basic/tokens/io/GETCHARToken.java
+++ src/de/doridian/crtdemo/basic/tokens/io/GETCHARToken.java
@@ -6,6 +6,6 @@ import de.doridian.crtdemo.basic.tokens.AbstractToken;
 public class GETCHARToken extends AbstractToken {
     @Override
     public String getCode(String prefix) {
-        return prefix + parametersSplitDetailed[parametersSplitDetailed.length - 1].subParams[0].getAsParameter() + " = $io.readChar();";
+        return prefix + parametersSplitDetailed[0].subParams[0].getAsParameter() + " = $io.readChar();";
     }
 }
