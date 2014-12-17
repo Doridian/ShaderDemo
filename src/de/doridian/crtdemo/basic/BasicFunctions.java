8e684611e36f94d9adee1c0ad1ba20739a276a7c
Mark Dietzer <mriq91@gmail.com>
2014-12-17 17:41:43 +0100

    Make APIs a bit more basic-esque

--- src/de/doridian/crtdemo/basic/BasicFunctions.java
+++ src/de/doridian/crtdemo/basic/BasicFunctions.java
@@ -14,30 +14,28 @@ public class BasicFunctions {
     public static String LEFT$(String str, int num) {
         return str.substring(0, num);
     }
-
     public static String RIGHT$(String str, int num) {
         return str.substring(str.length() - num, str.length());
     }
-
-    public static String CHR$(String str, int num) {
-        return "" + str.charAt(num);
+    public static String MID$(String str, int start, int len) {
+        return str.substring(start, start + len);
     }
 
-    public static int CHR(String str, int num) {
+    public static int ORD(String str, int num) {
         return str.charAt(num);
     }
-
-    public static String SUB$(String str, int start, int end) {
-        return str.substring(start, end);
+    public static int ORD(String str) {
+        return ORD(str, 0);
+    }
+    public static String CHR$(int chr) {
+        return "" + ((char)chr);
     }
 
     private final static Pattern _LTRIM = Pattern.compile("^\\s+");
     private final static Pattern _RTRIM = Pattern.compile("\\s+$");
-
     public static String LTRIM$(String s) {
         return _LTRIM.matcher(s).replaceAll("");
     }
-
     public static String RTRIM$(String s) {
         return _RTRIM.matcher(s).replaceAll("");
     }
@@ -45,7 +43,6 @@ public class BasicFunctions {
     public static int ARRAYLEN(Object[] array) {
         return array.length;
     }
-
     public static int ARRAYLEN(int[] array) {
         return array.length;
     }
